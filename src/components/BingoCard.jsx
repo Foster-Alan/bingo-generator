2f517ae6dea18d34df2fe0be87a19759fc0a6117
Patrick F <ptk.patrick.ferreiraa@gmail.com>
2025-11-14 20:58:53 -0300

    Atualização Basica

--- src/components/BingoCard.jsx
+++ src/components/BingoCard.jsx
@@ -1,9 +1,13 @@
 export default function BingoCard({ grid }) {
   return (
     <div className="card">
-      {grid.flat().map((img, i) => (
-        <img key={i} src={img} alt="" className="cell" />
-      ))}
+      <h2 className="card-title">Bingo</h2>
+
+      <div className="card-grid">
+        {grid.flat().map((img, i) => (
+          <img key={i} src={img} alt="" className="cell" />
+        ))}
+      </div>
     </div>
   );
 }
